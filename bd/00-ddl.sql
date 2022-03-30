7c437480dc2eaab5d514482f44163607ac7ac2d0
abrilchauq <abril.chauqueet12d1@gmail.com>
2022-03-30 18:15:31 -0300

    Create 00-ddl.sql

---
+++ bd/00-ddl.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS ForeverLand;
+
+CREATE DATABASE ForeverLand;
+
+CREATE TABLE ForeverLand.fichin(
+    idFichin INT NOT NULL,
+    nombre VARCHAR (45) NOT NULL,
+    lanzamiento YEAR NOT NULL,
+    precio DECIMAL (7,2) NOT NULL,
+    PRIMARY KEY (idFichin)
+);
+
+CREATE TABLE ForeverLand.cliente(
+    DNI INT UNSIGNED NOT NULL,
+    nombre VARCHAR (45) NOT NULL,
+    apellido VARCHAR (45) NOT NULL,
+    mail VARCHAR (50) NOT NULL,
+    tarjeta INT UNSIGNED NOT NULL,
+    PRIMARY KEY (DNI)
+);
+
+CREATE TABLE ForeverLand.recarga(
+    fechahora DATETIME NOT NULL,
+    monto DECIMAL (7,2) NOT NULL,
+    DNI INT UNSIGNED NOT NULL,
+    PRIMARY KEY (fechahora,DNI),
+    CONSTRAINT FK_recarga_cliente FOREIGN KEY (DNI)
+        REFERENCES ForeverLand.cliente (DNI)
+);
+
+CREATE TABLE ForeverLand.transaccion(
+    fechahora DATETIME NOT NULL,
+    DNI INT UNSIGNED NOT NULL,
+    credito DECIMAL (7,2) NOT NULL,
+    idFichin INT NOT NULL,
+    PRIMARY KEY (fechahora,DNI),
+    CONSTRAINT FK_transaccion_fichin FOREIGN KEY (idFichin)
+        REFERENCES ForeverLand.fichin (idFichin),
+    CONSTRAINT FK_transaccion_cliente FOREIGN KEY (DNI)
+        REFERENCES ForeverLand.cliente (DNI)
+);
