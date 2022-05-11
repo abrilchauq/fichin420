6d65c8e18040f89b5b93f74cf0fa76bb813b488b
abrilchauq <abril.chauqueet12d1@gmail.com>
2022-05-11 17:48:43 -0300

    Update dump.sql

---
+++ bd/dump.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO registrarCliente(24567892, 'Fabian', 'Perez', 'fabianpeerez@gmail.com', 34254567886, )
+
+
+INSERT INTO AltaRecarga('23/03/2021 13:30:20', 50.4, 24567892);
+                        
